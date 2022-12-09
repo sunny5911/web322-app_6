1f8e9f74e2c405e6918db1145a5b3ed5f810f8ea
sunny@5911 <sunny236946@gmail.com>
2022-12-09 22:32:47 +0530

    update

--- server.js
+++ server.js
@@ -1,3 +1,14 @@
+/********************************************************************************* 
+*  WEB322 – Assignment 6 
+*  I declare that this assignment is my own work in accordance with Seneca  Academic Policy.   
+*  No part of this assignment has been copied manually or electronically from any other source 
+*  (including web sites) or distributed to other students. 
+*  
+*  Name: Sunny  Student ID: 128365210 Date: 10-12-2022
+* 
+*  Online (Cyclic) URL: https://red-troubled-tortoise.cyclic.app/
+* 
+********************************************************************************/
 var HTTP_PORT = process.env.PORT || 8080;
 var express = require("express");
 var app = express();
